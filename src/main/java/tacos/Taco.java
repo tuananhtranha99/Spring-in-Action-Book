3f214b2f7f7f16832d777d2f5e2cdebef7a8b956
tuananhtranha99 <anhtht@vnpt-technology.vn>
2023-07-29 17:37:57 +0700

    commit July 29th 2023

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,12 +1,19 @@
 package tacos;
 
+import java.util.Date;
 import java.util.List;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class Taco {
+	
+	private Long id;
+	
+	private Date createdAt;
 	
 	@NotNull
 	@Size(min=5, message="Name must be at least 5 characters long")
