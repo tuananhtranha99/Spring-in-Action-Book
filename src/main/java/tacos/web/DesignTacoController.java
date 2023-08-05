a1819a298fdbcccb5d8dc3c2024d1db527894c94
tuananhtranha99 <anhtht@vnpt-technology.vn>
2023-08-05 20:37:04 +0700

    update  August 5th 2023

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -10,12 +10,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.Order;
 import tacos.Taco;
 import tacos.data.IngredientRepository;
 import tacos.data.TacoRepository;
@@ -23,10 +26,21 @@ import tacos.data.TacoRepository;
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
 
+	@ModelAttribute(name = "order")
+	public Order order() {
+		return new Order();
+	}
+
+	@ModelAttribute(name = "taco")
+	public Taco taco() {
+		return new Taco();
+	}
+
 	private final IngredientRepository ingredientRepo;
-	
+
 	private TacoRepository designRepo;
 
 	public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository designRepo) {
@@ -47,12 +61,12 @@ public class DesignTacoController {
 	}
 
 	@PostMapping
-	public String processDesign(@Valid Taco design, Errors errors) {
+	public String processDesign(@Valid Taco design, Errors errors, @ModelAttribute Order order) {
 		if (errors.hasErrors()) {
 			return "design";
 		}
-		// Save the taco design...
-		// We'll do this in chapter 3
+		Taco saved = designRepo.save(design);
+		order.addDesign(saved);
 		log.info("Processing design: " + design);
 		return "redirect:/orders/current";
 	}
